7c04f22fc0f22033a2e0ed69cc4266033e44dc6d
enrichoalkalas01 <enrichoalkalas01@gmail.com>
2022-02-10 13:31:35 +0700

    add changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [Modal, setModal] = useState(false)
+
+    const ClickButton = () => {
+        setModal(true)
+    }
+
+    const ModalClick = (e) => {
+        if ( e.target !== e.currentTarget ) return true
+        else setModal(false)
+    }
+
+    return(
+        <>
+            <section id="app">
+                <h1>Text For Tester Modal Here</h1>
+                <button onClick={ ClickButton } id="modal-button">Test Modal Here</button>
+                <div id="modal-box" className={ Modal ? "active" : "" }>
+                    <div className="wrapper-box" onClick={ ModalClick }>
+                        <div className="wrapper-content">
+                            Tester
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
 }
 
-export default App;
+export default App
